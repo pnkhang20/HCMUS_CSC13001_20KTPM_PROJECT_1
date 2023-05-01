1d4715792ad331935c58eea3fe283967fb0eac5d
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-05-01 20:50:54 +0700

    import data into dashboard

--- BookstoreApi/Management/ViewModels/HomeViewModel.cs
+++ BookstoreApi/Management/ViewModels/HomeViewModel.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using Management.Cores;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using Management.Models;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using Management.Views;
+using System.ComponentModel;
 
 namespace Management.ViewModels
 {
-    class HomeViewModel
+    class HomeViewModel:ObservableObject
     {
+        private const string BookApiUrl = "https://localhost:7122/api/Books";
+        private readonly HttpClient httpClient = new HttpClient();
+        public ObservableCollection<Book> Books { get; } = new ObservableCollection<Book>();
+
+        public int _total = 0;
+        public int Total
+        {
+            get { return _total; }
+            set
+            {
+                _total = value;
+            }
+        }
+        public HomeViewModel()
+        {
+          
+            LoadBooks();
+           
+            // Wire up the SearchText property
+        }
+
+        public async Task LoadBooks()
+        {
+            try
+            {
+               
+
+                var urlBuilder = new StringBuilder(BookApiUrl);
+                var response = await httpClient.GetAsync(urlBuilder.ToString());
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var content = await response.Content.ReadAsStringAsync();
+                    var books = JsonConvert.DeserializeObject<List<Book>>(content);
+               
+                    Total = books.Count;
+
+                  
+                    foreach (var book in books)
+                    {
+                        if (book.Quantity >= 0 && book.Quantity < 5)
+                        {
+                            Books.Add(book);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
     }
 }
